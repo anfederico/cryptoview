6e31f69299bb459f2443770c77b74e52065f3546
rbn920 <robby@nelson-wilcox.com>
2018-01-01 17:56:29 -0800

    Testing settings set

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,11 +1,11 @@
 # Mongo
-mongo_server    = 'ds184293.mlab.com'   # Find this in your deployment settings
-mongo_id        = 84293                 # Find this in your deployment settings
-mongo_client    = 'cryptoview'          # The name of your deployment
-mongo_user      = 'anfederico'          # The username you added as a user to your deployment
-mongo_pass      = 'futureth'            # The password you made for that user
-mongo_portfolio = 'anthony'             # A trivial name you can make it anything
- 
+mongo_server    = 'ds042888.mlab.com'   # Find this in your deployment settings
+mongo_id        = 42888                 # Find this in your deployment settings
+mongo_client    = 'crypt2'          # The name of your deployment
+mongo_user      = 'testing'          # The username you added as a user to your deployment
+mongo_pass      = 'x'            # The password you made for that user
+mongo_portfolio = 'robby'             # A trivial name you can make it anything
+
 # Supported Keys
 poloniex_key    = ''
 poloniex_secret = ''
@@ -16,6 +16,7 @@ bittrex_secret  = ''
 gdax_key        = ''
 gdax_secret     = ''
 gdax_passphrase = ''
- 
+
 # Only list exchanges you have keys for
-exchanges = ['gdax', 'bittrex', 'binance', 'poloniex']
+#exchanges = ['gdax', 'bittrex', 'binance', 'poloniex']
+exchanges = []
